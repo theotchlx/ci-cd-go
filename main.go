0b9cc29334446ac69b4590e8809eddcf7b7cccef
hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>
2024-04-30 15:35:33 +0200

    feat: adding log

--- main.go
+++ main.go
@@ -1,20 +1,22 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
+	"fmt"
+	"log"
+	"net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello les ziGOtos ;)...... :-|")
+	message := "Hello les ziGOtos ;)...... :-|"
+	fmt.Println(message)
+	fmt.Fprintf(w, message)
 }
 
 func main() {
-    http.HandleFunc("/", handler)
-    err := http.ListenAndServe(":8080", nil)
-    if err != nil {
-        log.Fatalf("Error starting server: %v", err)
-    }
+	http.HandleFunc("/", handler)
+	fmt.Println("Main launch")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatalf("Error starting server: %v", err)
+	}
 }
-
